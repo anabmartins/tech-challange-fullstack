81439af781f11b5c7418b5b8ca786abb2ba388d2
anabmartins <ana_beatriz-martins@estudante.sesisenai.org.br>
2024-02-05 22:11:39 -0300

    build: redux toolkit dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,16 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
+import { createBrowserRouter, 
+RouterProvider } from "react-router-dom";
 import Manage from "./screens/Manage";
 import SignIn from "./screens/SignIn";
 import SignUp from "./screens/SignUp";
 
 const router = createBrowserRouter([
-  {
-    path: "",
-    element: <SignIn />
-  },
-  {
-    path: "/signup",
-    element: <SignUp />
-  },
-  {
-    path: "/manage",
-    element: <Manage />
-  }
+  { path: "/", element: <SignIn /> },
+  { path: "/signup", element: <SignUp /> },
+  { path: "/manage", element: <Manage /> }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
