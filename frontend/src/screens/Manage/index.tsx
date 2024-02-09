ddd377180328dba70d0682138cd8dc13b1a25a0a
Anabe <99266498+anabmartins@users.noreply.github.com>
2024-02-09 16:33:02 -0300

    fear: getUser function in manage page

--- frontend/src/screens/Manage/index.tsx
+++ frontend/src/screens/Manage/index.tsx
@@ -1,4 +1,18 @@
+import { useState } from 'react'
+
+function getUser(){
+ let user = local storage.getItem('user')
+if(user){
+   user = JSON.parse(user)
+  } else {
+  user = null
+  }
+return user;
+}
+
 const Manage = () => {
+
+const {user, setUser} = useState(getUser());
   return (
     <>
       <div className="main">
