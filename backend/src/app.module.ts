5323d5e2b03b108e885055ee002e0335d4226da4
anabmartins <ana_beatriz-martins@estudante.sesisenai.org.br>
2024-02-09 09:50:17 -0300

    feat: auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { VehicleModule } from './vehicle/vehicle.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: ['.env.development.local'],
   }),
   UserModule,
-  VehicleModule],
+  VehicleModule,
+  AuthModule],
   controllers: [],
   providers: [],
 })
