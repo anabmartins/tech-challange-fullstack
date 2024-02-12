19c21c80e5638d4c096b541b999e2927d331f599
anabmartins <ana_beatriz-martins@estudante.sesisenai.org.br>
2024-02-12 15:14:28 -0300

    feat: manage screen

--- frontend/src/screens/Manage/index.tsx
+++ frontend/src/screens/Manage/index.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import LogoutIcon from "@mui/icons-material/Logout";
+import User from "/user.svg";
 
 // set the user logged in localstorage
 function getUser() {
   let user = localStorage.getItem("user");
-  if (user) user = JSON.parse(user); else user = null;
+  if (user) user = JSON.parse(user);
+  else user = null;
   console.log(user);
   return user;
 }
@@ -12,30 +15,41 @@ function getUser() {
 const Manage = () => {
   const [user, setUser] = useState(getUser());
   const navigate = useNavigate();
-  
+
   // logout user
   const handleLogout = () => {
     localStorage.removeItem("user");
     setUser(null);
-    navigate('/')
+    navigate("/");
   };
 
   return (
     <>
+      {/* {user && ( */}
+      {/* <> */}
       <div className="main">
+        <div className="user-sec">
+          <p className="userName">
+            Olá, User
+            <button className="logout" onClick={handleLogout}>
+              sair
+              <LogoutIcon />
+            </button>
+          </p>
+          <img src={User} />
+        </div>
+        {/* </> */}
+        {/* )} */}
         <div className="card">
           <img src="/icon-park_car.svg" className="icon" alt="" />
           <h1 className="title">Gerenciamento</h1>
           <div className="line"></div>
-
-          {user && (
-            <>
-              <p className="userName">Olá, {user.name}</p>
-              <button className="logout" onClick={handleLogout}>
-                sair
-              </button>
-            </>
-          )}
+          <p className="add-sec">
+            <img src="/plus.svg" />
+            adicionar
+          </p>
+          <p className="subtitle">Lista de veículos</p>
+          <div className="list"></div>
         </div>
       </div>
     </>
